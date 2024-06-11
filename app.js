2230df8cc50af6fd724806a4f770ab925b73d240
송이현 <songehyun00@gmail.com>
2024-06-11 10:55:31 +0900

    객체 동적 할당

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const a = () => {
+const a = (a, b, c) => {
   return {
-    name: "",
-    age: 0,
-    job: "",
+    name: a,
+    age: b,
+    job: c,
   };
 };
 // console.log(a());
 
-const b = a();
+const b = a("송이현", 31, "학생");
 
-console.log(b);
+console.log("첫번째 b:", b);
+b.address = "대전";
+console.log("두번째 b:", b);
